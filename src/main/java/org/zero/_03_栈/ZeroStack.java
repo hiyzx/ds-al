08ca0b97d5cd0895261391c4392384f27b401afe
shuihan <shuihan@gaoding.com>
2020-06-12 18:20:03 +0800

    注释修改

--- src/main/java/org/zero/_03_栈/ZeroStack.java
+++ src/main/java/org/zero/_03_栈/ZeroStack.java
@@ -11,10 +11,14 @@ import java.util.List;
  */
 public class ZeroStack<E> {
 
-    // 用集合来实现栈
+    /**
+     * 用集合来实现栈
+     */
     private List<E> list = new ArrayList<>();
 
-    // 移动栈顶元素
+    /**
+     * 移动栈顶元素
+     */
     public void pop() {
         if (list.isEmpty()) {
             return;
@@ -22,12 +26,16 @@ public class ZeroStack<E> {
         list.remove(list.size() - 1);
     }
 
-    // 往栈顶添加元素
+    /**
+     * 往栈顶添加元素
+     */
     public void push(E e) {
         list.add(e);
     }
 
-    // 获取栈顶元素
+    /**
+     * 获取栈顶元素
+     */
     public E top() {
         if (list.isEmpty()) {
             return null;
@@ -35,12 +43,16 @@ public class ZeroStack<E> {
         return list.get(list.size() - 1);
     }
 
-    // 判断栈是否为空
+    /**
+     * 判断栈是否为空
+     */
     public Boolean isEmpty() {
         return list.size() == 0;
     }
 
-    // 返回栈元素数量
+    /**
+     * 返回栈元素数量
+     */
     public Integer size() {
         return list.size();
     }
